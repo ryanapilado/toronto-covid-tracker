e559fb2f1db39d9aaf1db2c86a8e37a69d5e6e6b
Ryan Apilado <ryan.apilado@gmail.com>
2020-07-28 17:54:28 -0400

    implement new case parsing

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const pdfjs = require('pdfjs-dist');
+const pdfjs = require('pdfjs-dist/es5/build/pdf.js');
 const wget = require('wget-improved');
 const fs = require('fs');
-const jsdom = require('jsdom');
 
 /**
  * Responds to any HTTP request.
@@ -10,27 +9,42 @@ const jsdom = require('jsdom');
  * @param {!express:Response} res HTTP response context.
  */
 exports.helloWorld = (req, res) => {
-  const src = 'https://files.ontario.ca/moh-covid-19-report-en-2020-07-16.pdf';
+  let dateString = new Date().toISOString().slice(0, 10).slice(0, 10);
+  const src = `https://files.ontario.ca/moh-covid-19-report-en-${dateString}.pdf`;
   const output = '/tmp/report.pdf';
 
-  const options = {};
-
-  let download = wget.download(src, output, options);
+  let download = wget.download(src, output);
   download.on('error', function(err) {
     console.log(err);
     res.status(400).send(message);
   });
-  download.on('end', function(output) {
-    console.log(output);
+
+  download.on('end', async function(output) {
+
     const contents = fs.readFileSync('/tmp/report.pdf', {encoding: 'binary'});
 
-    const loadingTask = pdfjs.getDocument({data: contents});
-    loadingTask.promise.then(function(pdf) {
-      console.log('PDF loaded');
-      console.log(pdf);
-      let message = req.query.message || req.body.message || 'Hello World!';
-      res.status(200).send(message);
+    pdfjs.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/build/pdf.worker.js'
+    let pdf = await pdfjs.getDocument({data: contents}).promise.then();
+
+    async function getNewCases(pageNumber, healthUnit, offset) {
+      let page = await pdf.getPage(pageNumber);
+      let content = await page.getTextContent();
+      let idx = content.items.findIndex(e => e.str === healthUnit);
+      return content.items[idx + offset].str;
+    }
+
+    let torontoNewCases = await getNewCases(10, 'Toronto Public Health', 4);
+    let ontarioNewCases = await getNewCases(11, 'TOTAL ONTARIO', 4);
+    console.log(torontoNewCases);
+    console.log(ontarioNewCases);
+    res.status(200).send({
+      "torontoNewCases": torontoNewCases,
+      "ontarioNewCases": ontarioNewCases
     });
-  });
+
+    }, function (reason) {
+      console.error(reason);
+    }
+  );
 };
 
