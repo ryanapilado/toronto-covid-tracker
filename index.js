5ff857671145c01b013ad8641d656cc569ed65cb
Ryan Apilado <ryan.apilado@gmail.com>
2020-07-28 18:05:24 -0400

    pass date in req body

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const fs = require('fs');
  * @param {!express:Response} res HTTP response context.
  */
 exports.helloWorld = (req, res) => {
-  let dateString = new Date().toISOString().slice(0, 10).slice(0, 10);
-  const src = `https://files.ontario.ca/moh-covid-19-report-en-${dateString}.pdf`;
+  console.log(req);
+  const src = `https://files.ontario.ca/moh-covid-19-report-en-${req.query.date}.pdf`;
   const output = '/tmp/report.pdf';
 
   let download = wget.download(src, output);
   download.on('error', function(err) {
     console.log(err);
-    res.status(400).send(message);
+    res.status(400).send(err);
   });
 
   download.on('end', async function(output) {
